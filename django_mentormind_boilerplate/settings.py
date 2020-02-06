1b42f2c37262c2586d1457a63562bd15d28abbbf
umangrana <58718300+umangrana@users.noreply.github.com>
2020-02-06 13:36:25 +0530

    Update settings.py

--- django_mentormind_boilerplate/settings.py
+++ django_mentormind_boilerplate/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'django_mentormind_boilerplate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'TuitionWala',
-        'USER': 'root',
-        'PASSWORD': 'umangrana123',
-        'PORT': '3306',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
